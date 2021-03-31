c88cb1e84f7a2a3ba6d84e1b53ebaec170e7d798
Otavio Henrique <OtavioHenLimSil@gmail.com>
2021-03-31 20:38:49 -0300

    adsasd

--- 02-curso-rxjs/01-funcoes/basico_2.js
+++ 02-curso-rxjs/01-funcoes/basico_2.js
@@ -6,6 +6,7 @@ const boaTarde = function() {
   console.log('Boa tarde!');
 };
 
+// 1) Passar uma funcao como parametro para outra funcao
 function executarQualquerCoisa(fn) {
   if (typeof fn === 'function') {
     fn();
@@ -15,3 +16,21 @@ function executarQualquerCoisa(fn) {
 executarQualquerCoisa(3);
 executarQualquerCoisa(bomDia);
 executarQualquerCoisa(boaTarde);
+
+// 2) Retornar uma funcao a partir de outra funcao
+
+// function potencia(base, expoente) {
+//   return Math.pow(base, expoente);
+// }
+// const bits8 = potencia(2, 8);
+// console.log(bits8);
+
+function potencia(base) {
+  return function(expoente) {
+    return Math.pow(base, expoente);
+  }
+}
+const potenciaDe2 = potencia(2);
+console.log(potenciaDe2(8));
+const resultadoPotencia_3_4 = potencia(3)(4);
+console.log(resultadoPotencia_3_4);
